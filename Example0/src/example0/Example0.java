ad89193302128648607c73661d2422ca0e6a4a77
وحید خزایی نژاد <38763392+vahid-khazaei-nezhad@users.noreply.github.com>
2021-03-10 13:47:42 +0330

    It's just a simple project to start with.
    It's just a simple project to start with.

---
+++ Example0/src/example0/Example0.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package example0;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Khazaeinezhad_Vahid
+ */
+public class Example0 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Welcome to Java");
+        
+    }//end of main() method
+    
+}//end of Calss
