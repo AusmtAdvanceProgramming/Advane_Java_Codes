eac74b4d70882612d48507fe0de6fb25abc85758
وحید خزایی نژاد <38763392+vahid-khazaei-nezhad@users.noreply.github.com>
2021-03-12 10:22:08 +0330

    ایجاد پکیج اختصاصی برای هر تیم

---
+++ AUSMT_Winter_99/src/ausmt_winter_99/AusmtWinter99.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ausmt_winter_99;
+
+/**
+ *
+ * @author Khazaeinezhad_Vahid
+ */
+public class AusmtWinter99 {
+    
+    public static void main(String[] args){
+    
+        System.out.println("Welcome to Java");
+    
+    }//end of main method
+    
+}//end of Class
