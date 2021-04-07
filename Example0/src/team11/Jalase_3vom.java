490b135595ed4ea7db7bbfaf868331be3cf78c6c
Parsley21esi <80389550+Parsley21esi@users.noreply.github.com>
2021-04-07 16:09:44 +0430

    Jalase jadid ezafe shod

--- Example0/src/team11/Jalase_3vom.java
+++ Example0/src/team11/Jalase_3vom.java
@@ -75,6 +75,7 @@ public class Jalase_3vom {
         //End of Example 3
         */
         //Example 6
+        /*
         int numberOfYears;
         double annualInterestRate;
         double monthlyInterestRate;
@@ -102,7 +103,7 @@ public class Jalase_3vom {
                 (int) (monthlyPayment * 100) / 100.0);
         System.out.println("The total payment is " + 
                 (int) (totalPayment * 100) / 100.0);
-        
+        */
         
 }//end of main() method
     
