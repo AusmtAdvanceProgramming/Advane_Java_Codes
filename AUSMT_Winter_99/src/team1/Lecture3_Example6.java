fec50327ee3acebcd3748a753963e0e45c0b5159
Amirrasuli <samer.mazand@gmail.com>
2021-04-07 13:10:53 +0430

    Crate my dedicated package
    من بسته خودم را برای ارایه تمرینات برای تیم یک ایجاد نمودم.

---
+++ AUSMT_Winter_99/src/team1/Lecture3_Example6.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team1;
+
+/**
+ *
+ * @author Yaran
+ */
+public class Lecture3_Example6 {
+    
+    public static void main(String[] args){
+        
+        System.out.println("vvvvvv");
+    
+    }//end of main() method
+    
+}//end of Class
