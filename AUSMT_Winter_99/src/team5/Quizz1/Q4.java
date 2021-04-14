4152b86295031814a0ee5146f97e1eb728127748
Nika Taheri <nikathrr@gmail.com>
2021-04-15 00:28:32 +0430

    Team5
    پروژه 4 به سیستم پول خرد اصلاح شد
    .فرمول مالیات پروژه 5 اصلاح شده

--- AUSMT_Winter_99/src/team5/Quizz1/Q4.java
+++ AUSMT_Winter_99/src/team5/Quizz1/Q4.java
@@ -15,31 +15,56 @@ public class Q4 {
     
              Scanner input = new Scanner (System.in);
         
-        int amount,n1,n2,n3,n4,n5,n6;
+        int amount,cash,n1,n2,n3,n4,n5,n6;
         
         System.out.println("Please enter your amount(less than 100,000 Toman):");
         
         amount = input.nextInt();
         
-        System.out.println("__________________________________________");
-        n1=amount/50000;
-         System.out.println("Your amount includes "+n1+" times 50,000 Toman.");
+        System.out.println("How do you want your cash(press the numbers below)?");
+        System.out.print("1-----50,000$\n2-----10,000$\n3-----5,000$\n4-----2,000$\n5-----1,000$\n6-----500$\n");
         
-        n2=amount/10000;
-        System.out.println("Your amount includes "+n2+" times 10,000 Toman.");
+        cash = input.nextInt();
         
-        n3=amount/5000;
-        System.out.println("Your amount includes "+n3+" times 5,000 Toman.");
+        switch(cash){
+            case 1 :
+                n1=amount/50000;
+                System.out.println(n1+" to 50,000$");
+                break;
+                
+            case 2:    
+                n2=amount/10000;
+                System.out.println(n2+" to 10,000$");
+                break;
+                
+            case 3:
+                n3=amount/5000;
+                System.out.println(n3+" to 5,000$");
+                break;
+            
+            case 4:
+                n4=amount/2000;
+                System.out.println(n4+" to 2,000$");
+                break;
+                
+            case 5:
+                n5=amount/1000;
+                System.out.println(n5+" to 1,000$");
+                break;
+                
+            case 6:
+                n6=amount/500;
+                System.out.println(n6+" to 5000$");
+                break;
+                
+            default:System.out.println("Please try again.");
+
+
+
+
+
+        }
         
-        n4=amount/2000;
-        System.out.println("Your amount includes "+n4+" times 2,000 Toman.");
-        
-        n5=amount/1000;
-        System.out.println("Your amount includes "+n5+" times 1,000 Toman.");
-        
-        n6=amount/500;
-        System.out.println("Your amount includes "+n6+" times 500 Toman.");
-        System.out.println("__________________________________________");   
     }
     
         
--- AUSMT_Winter_99/src/team5/Quizz1/Q5.java
+++ AUSMT_Winter_99/src/team5/Quizz1/Q5.java
@@ -33,27 +33,27 @@ public class Q5 {
                         System.out.println("your total payment is:"+total+"$");
                     }
                     else if(income<=33950){
-                            tax=income*1.5;
+                            tax=8350*0.1+(income - 8350)*0.15;
                             total=income+tax;
                             System.out.println("your total payment is:"+total+"$");
                     }
                     else if(income<=82250){
-                           tax=income*2.5;
+                           tax=8350*0.1+(33950-8350)*0.15+(income-33950)*0.25;
                            total=income+tax;
                            System.out.println("your total payment is:"+total+"$");
                     }
                     else if(income<=171550){
-                            tax=income*2.8;
+                            tax=8350*0.1+(33950-8350)*0.15+(82250-33950)*0.25+(income-82250)*0.28;
                             total=income+tax;
                             System.out.println("your total payment is:"+total+"$");
                     }
                     else if(income<=372950){
-                           tax=income*3.3;
+                           tax=8350*0.1+(33950-8350)*0.15+(82250-33950)*0.25+(income-82250)*0.28+(income-171550)*0.33;
                            total=income+tax;
                            System.out.println("your total payment is:"+total+"$");
                     }
                     else {
-                            tax=income*3.5;
+                            tax=8350*0.1+(33950-8350)*0.15+(82250-33950)*0.25+(171550-82250)*0.28+(372950-171550)*0.33+(income-372950)*0.35;
                             total=income+tax;
                             System.out.println("your total payment is:"+total+"$");
                     }
@@ -67,27 +67,27 @@ public class Q5 {
                        System.out.println("your total payment is:"+total+"$");
                     }
                     else if(income<=67900){
-                            tax=income*1.5;
+                            tax=16700*0.1+(income-16700)*0.15;
                             total=income+tax;
                             System.out.println("your total payment is:"+total+"$");
                     }
                     else if(income<=137050){
-                            tax=income*2.5;
+                            tax=16700*0.1+(67900-16700)*0.15+(income-679000)*0.25;
                             total=income+tax;
                             System.out.println("your total payment is:"+total+"$");
                     }
                     else if(income<=208850){
-                            tax=income*2.8;
+                            tax=16700*0.1+(67900-16700)*0.15+(137050-679000)*0.25+(income-137050)*0.28;
                             total=income+tax;
                             System.out.println("your total payment is:"+total+"$");
                     }
                     else if(income<=372950){
-                            tax=income*3.3;
+                            tax=16700*0.1+(67900-16700)*0.15+(137050-679000)*0.25+(208850-137050)*0.28+(income-208850)*0.33;
                             total=income+tax;
                             System.out.println("your total payment is:"+total+"$");
                     }
                       else {
-                            tax=income*3.5;
+                            tax=16700*0.1+(67900-16700)*0.15+(137050-679000)*0.25+(208850-137050)*0.28+(372950-208850)*0.33+(income-372950)*0.35;
                             total=income+tax;
                             System.out.println("your total payment is:"+total+"$");
                     }
@@ -101,27 +101,27 @@ public class Q5 {
                        System.out.println("your total payment is:"+total+"$");
                     }
                     else if(income<=33950){
-                           tax=income*1.5;
+                           tax=8350*0.1+(income-8350)*0.15;
                            total=income+tax;
                            System.out.println("your total payment is:"+total+"$");
                     }
                     else if(income<=68525){
-                           tax=income*2.5;
+                           tax=8350*0.1+(33950-8350)*0.15+(income-33950)*0.25;
                            total=income+tax;
                            System.out.println("your total payment is:"+total+"$");
                     }
                     else if(income<=104425){
-                            tax=income*2.8;
+                            tax=8350*0.1+(33950-8350)*0.15+(68525-33950)*0.25+(income-68525)*0.28;
                             total=income+tax;
                             System.out.println("your total payment is:"+total+"$");
                     }
                     else if(income<=186475){
-                           tax=income*3.3;
+                           tax=8350*0.1+(33950-8350)*0.15+(68525-33950)*0.25+(104426-68525)*0.28+(income-104426)*0.33;
                            total=income+tax;
                            System.out.println("your total payment is:"+total+"$");
                     }
                     else{
-                        tax=income*3.5;
+                        tax=8350*0.1+(33950-8350)*0.15+(68525-33950)*0.25+(104426-68525)*0.28+(186475-104426)*0.33+(income-186475)*0.35;
                         total=income+tax;
                         System.out.println("your total payment is:"+total+"$");
                     }
@@ -135,27 +135,27 @@ public class Q5 {
                        System.out.println("your total payment is:"+total+"$");
                     }
                     else if(income<=45500){
-                            tax=income*1.5;
+                            tax=11950*0.1+(income-11950)*0.15;
                             total=income+tax;
                             System.out.println("your total payment is:"+total+"$");
                     }
                     else if(income<=117450){
-                           tax=income*2.5;
+                           tax=11950*0.1+(45500-11950)*0.15+(income-45500)*0.25;
                            total=income+tax;
                            System.out.println("your total payment is:"+total+"$");
                     }
                     else if(income<=190200){
-                            tax=income*2.8;
+                            tax=11950*0.1+(45500-11950)*0.15+(117450-45500)*0.25+(income-117450)*0.28;
                             total=income+tax;
                             System.out.println("your total payment is:"+total+"$");
                     }
                     else if(income<=372950){
-                            tax=income*3.3;
+                            tax=11950*0.1+(45500-11950)*0.15+(117450-45500)*0.25+(190200-117450)*0.28+(income-192200)*0.33;
                             total=income+tax;
                             System.out.println("your total payment is:"+total+"$");
                     }
                     else{
-                        tax=income*3.5;
+                        tax=11950*0.1+(45500-11950)*0.15+(117450-45500)*0.25+(190200-117450)*0.28+(372950-192200)*0.33+(income-37250)*0.35;
                         total=income+tax;
                         System.out.println("your total payment is:"+total+"$");
                     }
