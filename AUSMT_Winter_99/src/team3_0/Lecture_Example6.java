a4b275c16062337920ee764003b72d834cd5b91a
amirali ghaumi <79565747+amiralighaumi@users.noreply.github.com>
2021-04-15 01:00:58 +0430

    team3
    refactor name

--- AUSMT_Winter_99/src/team3_0/Lecture_Example6.java
+++ AUSMT_Winter_99/src/team3_0/Lecture_Example6.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ausmt_winter_99.team3;
+package team3_0;
 
 import java.util.Scanner;
 
