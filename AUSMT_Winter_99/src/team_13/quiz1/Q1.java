cabe6f6fa83f0995767c4de192841d372e127e9c
Parvin-Akbari <radparinaz903@gmail.com>
2021-04-14 14:02:54 -0700

    team13 + workshop2
    چهارتا کد تمرین بعلاوه کارگاه رو نوشتم

---
+++ AUSMT_Winter_99/src/team_13/quiz1/Q1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team_13.quiz1;
+
+/**
+ *
+ * @author matrix
+ */
+public class Q1 {
+    
+    public static void main(String[] args){
+        //کدهاتو اینجا می زاری
+    }//end of main() method
+    
+}//end of class
---
+++ AUSMT_Winter_99/src/team_13/quiz1/Q2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team_13.quiz1;
+
+/**
+ *
+ * @author matrix
+ */
+public class Q2 {
+public static void main(String[] args){
+    
+    }//end of main() method
+    
+}//end of class
