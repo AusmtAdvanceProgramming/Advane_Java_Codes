1676d5e6653dffd29ac5f0193c034294f2222458
Sina-2020 <82501800+Sina-2020@users.noreply.github.com>
2021-04-14 21:50:27 +0430

    team10Package.Quiz1
    بسته تیم 10 ایجاد شد
    توسط سینا قلی زاده و امیر رسولی

--- AUSMT_Winter_99/src/team10/quiz1/Q0.java
+++ AUSMT_Winter_99/src/team10/quiz1/Q0.java
@@ -1,23 +1,19 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Q0.java
  */
-package team10.quiz1;
+package team10.quiz1 ;
 
-/**
- *
- * @author Rayan
- */
+ // 1. Sina Gholizadeh 2.Amir Rasuli
+ 
 public class Q0 {
-    
-    public static void main(String[] args){
-    
-    //;دهاتونو اینجا می نویسید
-    
-    
-    
-    }//end of main() method
-    
-    
-}//end of clas
+    public static void main (String[] args) {
+        /*
+        Error0 = dar Method main kalame "static" vojod ndrd
+        Error1 = در کیس پایین باید کلمه مین هم  وجود داشته باشد ( main )
+        Error2 =  کوتیشن باید دابل کوتیشن شود
+        Error3 : there is no "double quotation" in "System.out.println" parenthesis ;
+        Error3 = در خروجی گرفتن در پرانتز هیچ دابل کوتیشینی وجود ندار
+        Error4 = از براکت ها بجای پرانتز در اخر کلاس ها استفاده میشود
+        */
+    }    
+}
--- AUSMT_Winter_99/src/team10/quiz1/Q1.java
+++ AUSMT_Winter_99/src/team10/quiz1/Q1.java
@@ -1,14 +1,61 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Q1.java.tamrin01
  */
-package team10.quiz1;
-
-/**
- *
- * @author Rayan
- */
+package team10.quiz1 ;
+// 1.Sina gholizadeh 2.Amir Rasuli
+import java.util.Scanner ;
+//***
 public class Q1 {
-    
+    public static void main (String[] args) {
+        Scanner input=new Scanner (System.in) ;
+        //***
+        double x , y ;
+        double a , b , e ;
+        double c , d , f ;
+        //***
+        System.out.println ("____________________") ;
+        
+        System.out.println ("ax+by=e") ;
+        
+        System.out.println ("cx+dy=f") ;
+        
+        System.out.println ("__________") ;
+        
+        System.out.println ("meqdar haye zir ra vared konid : ") ;
+        
+        System.out.print ("a : ") ;
+        
+        a=input.nextDouble () ;
+        
+        System.out.print ("b : ") ;
+        
+        b=input.nextDouble () ;
+        
+        System.out.print ("e : ") ;
+        
+        e=input.nextDouble () ;
+        
+        System.out.print ("c : ") ;
+        
+        c=input.nextDouble () ;
+        
+        System.out.print ("d : ") ;
+        
+        d=input.nextDouble () ;
+        
+        System.out.print ("f : ") ;
+        
+        f=input.nextDouble () ;
+        
+        System.out.println ("__________") ;
+        
+        //***
+        x=((c*e)-(b*f))/((a*e)-(b*d)) ;
+        
+        y=((a*f)-(d*c))/((a*e)-(b*d)) ;
+        //***
+        System.out.println ("x="+x+"\ny="+y) ;
+        
+        System.out.println ("____________________") ;
+    }
 }
