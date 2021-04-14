210b8032bcba742c333dd9ceab11a0333840cb02
vernamusic <68544155+vernamusic@users.noreply.github.com>
2021-04-14 23:56:11 +0430

    team0quiz1
    فایل های کویز دوباره اپلود شد

--- AUSMT_Winter_99/src/team0quiz1/Q1.java
+++ AUSMT_Winter_99/src/team0quiz1/Q1.java
@@ -1,14 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package team0quiz1;
 
-/**
- *
- * @author Verna
- */
+import java.util.Scanner ;
+
+ //author Amirhossein Ghaffari
+ //author Ashkan Rasouli
+ 
 public class Q1 {
-    
+    public static void main (String[] args) {
+        Scanner input=new Scanner (System.in) ;
+       
+        double x , y ;
+        double a , b , e ;
+        double c , d , f ;
+        
+       
+        System.out.println ("____________________") ;
+        System.out.println ("ax+by=e") ;
+        System.out.println ("cx+dy=f") ;
+        System.out.println ("__________") ;
+        System.out.println ("Enter the following values : ") ;
+        System.out.print ("a : ") ;
+        a=input.nextDouble () ;
+        System.out.print ("b : ") ;
+        b=input.nextDouble () ;
+        System.out.print ("e : ") ;
+        e=input.nextDouble () ;
+        System.out.print ("c : ") ;
+        c=input.nextDouble () ;
+        System.out.print ("d : ") ;
+        d=input.nextDouble () ;
+        System.out.print ("f : ") ;
+        f=input.nextDouble () ;
+        System.out.println ("__________") ;
+        
+       
+        x=((c*e)-(b*f))/((a*e)-(b*d)) ;
+        y=((a*f)-(d*c))/((a*e)-(b*d)) ;
+        
+     
+        System.out.println ("x="+x+"\ny="+y) ;
+        System.out.println ("____________________") ;
+    }
 }
