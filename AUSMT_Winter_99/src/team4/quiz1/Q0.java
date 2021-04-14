b2316d0e4edffab3cfeb9ab996dd96f7738d5d99
Kamyab Ashouri <80399697+KamyabAshouri@users.noreply.github.com>
2021-04-14 16:15:17 +0430

    Team4.Quiz1
    quiz1 : Q0 , Q1 , Q2 , Q3 , Q4 ;
    team4
    @author Kamyab Ashouri
    @author Fatemeh Naiji

---
+++ AUSMT_Winter_99/src/team4/quiz1/Q0.java
@@ -0,0 +1,20 @@
+/*
+ * Q0.java
+ */
+package team4.quiz1 ;
+/**
+ * @author Kamyab Ashouri
+ * @author Fatemeh Naiji
+ */
+public class Q0 {
+    public static void main (String[] args) {
+        /*
+        Error0 : there is no "static" in the "main" method ;
+        Error1 : the "main" word should be in lower case ;
+        Error2 : the "quotation" should be "double quotation" ;
+        Error3 : there is no "double quotation" in "System.out.println" parenthesis ;
+        Error4 : use "brackets" instead of "parentheses" in the end of the class ;
+        Error5 :
+        */
+    }    
+}
