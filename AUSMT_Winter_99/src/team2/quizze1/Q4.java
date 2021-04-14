b06600661bf2ca11984ecaf0ef82d4918fde0d0e
hniehadian <79898625+hniehadian@users.noreply.github.com>
2021-04-14 23:35:14 +0430

    Update Q4.java

--- AUSMT_Winter_99/src/team2/quizze1/Q4.java
+++ AUSMT_Winter_99/src/team2/quizze1/Q4.java
@@ -10,63 +10,99 @@ public class Q4 {
     
     public static void main(String[] args) {
     
-    int money;
-    int Amounts;
-    int money1,money2,money3,money4,money5,money6;
     
-    
-    Scanner input = new Scanner (System.in);
-    
-    System.out.print("Enter the amount(The amount entered is less than $ 100,000) :");
-    money = input.nextInt();
-   
+        double pay;
         
-    System.out.print("How much money do you want to spend?:(Enter 0 if you want to exit the program)"
-              + "\n [1]-50000$ \n [2]-10000 \n [3]-5000 \n [4]-2000 \n [5]-1000 \n [6]-500" );
-    
-    Amounts = input.nextInt();
-    
-    switch(Amounts) {
         
-        case 1 :
-            money1=money/50000;
-            System.out.print(money1);
-            System.out.print("to $ 50000");
-            break;
-            
-       case 2 :
-            money2=money/10000;
-            System.out.print(money2);  
-            System.out.print("to $ 10000"); 
-            break;
-            
-        case 3 :
-            money3=money/5000;
-            System.out.print(money3); 
-            System.out.print("to $ 5000"); 
-            break;
-            
-        case 4 :
-            money4=money/2000;
-            System.out.print(money4); 
-            System.out.print("to $ 500");
-            break;
-            
-        case 5 :
-            money5=money/1000;
-            System.out.print(money5); 
-            System.out.print("to $ 1000"); 
-            break;
-            
-        case 6 :
-            money6=money/500;
-            System.out.print(money6); 
-            System.out.print("to $ 500"); 
-            break;
-              
-                    
-     }   
-   }
- } //The end
-
-
+        int cahngecounter1=0;
+        int cahngecounter2=0;
+        int cahngecounter3=0;
+        int cahngecounter4=0;
+        int cahngecounter5=0;
+        int cahngecounter6=0;
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter your pay, make it less than 100,000 tomans:");
+        
+        pay = input.nextDouble();
+        
+           if (pay % 50000 == 0){
+              pay /=50000;
+              System.out.println(" your chnage in 50000 = " + pay);  
+           }
+           else if (pay % 50000 != 0){
+            while (pay > 50000){
+                   pay -=50000;
+                   cahngecounter1++;
+               }
+            System.out.println("your change in 50000 = " + cahngecounter1);
+           }
+        
+           if (pay % 10000 == 0){
+              pay /=10000;
+              System.out.println(" your chnage in 10000 = " + pay);  
+           }
+           else if (pay % 10000 != 0){
+            while (pay > 10000){
+                   pay -=10000;
+                   cahngecounter2++;
+               }
+            System.out.println("your change in 10000 = " + cahngecounter2);
+           }
+        
+        
+           if (pay % 5000 == 0){
+              pay /=5000;
+              System.out.println(" your chnage in 5000 = " + pay);  
+           }
+           else if (pay % 5000 != 0){
+            while (pay > 5000){
+                   pay -=5000;
+                   cahngecounter3++;
+               }
+            System.out.println("your change in 5000 = " + cahngecounter3);
+           }
+        
+           if (pay % 2000 == 0){
+              pay /=2000;
+              System.out.println(" your chnage in 2000 = " + pay);  
+           }
+           else if (pay % 2000 != 0){
+            while (pay > 2000){
+                   pay -=2000;
+                   cahngecounter4++;
+               }
+            System.out.println("your change in 1000 = " + cahngecounter4);
+           }
+        
+        
+        
+           if (pay % 1000 == 0){
+              pay /=1000;
+              System.out.println(" your chnage in 1000 = " + pay);  
+           }
+           else if (pay % 1000 != 0){
+            while (pay > 1000){
+                   pay -=1000;
+                   cahngecounter5++;
+               }
+            System.out.println("your change in 1000 = " + cahngecounter5);
+           }
+        
+        
+       
+           if (pay % 500 == 0){
+              pay /=500;
+              System.out.println(" your chnage in 500 = " + pay);  
+           }
+           else if (pay % 500 != 0){
+            while (pay > 500){
+                   pay -=500;
+                   cahngecounter6++;
+               }
+            System.out.println("your change in 500 = " + cahngecounter6);
+           }
+        
+    }
+    
+}
