1023f515f228e9e03d8adbe19216093aa3859073
HosseinRahi <79898697+HosseinRahi@users.noreply.github.com>
2021-04-21 16:27:52 +0430

    team1
    workshop3_2

--- AUSMT_Winter_99/src/team2/workshop3_2.java
+++ AUSMT_Winter_99/src/team2/workshop3_2.java
@@ -13,30 +13,30 @@ public class workshop3_2 {
         Scanner input=new Scanner (System.in) ;
         //*
         String series ;
-        char letter ;
+        char word ;
         int length ;
         int counter=0 ;
         int i=0 ;
         //*
         System.out.println ("________") ;
-        System.out.print ("Enter your desired \"String\" please : ") ;
+        System.out.print ("Enter your \"String\" please : ") ;
         series=input.next () ;
         System.out.println ("____") ;
-        System.out.print ("Enter your desired \"Letter\" please : ") ;
-        letter=input.next ().charAt (0) ;
+        System.out.print ("Enter your \"word\" please : ") ;
+        word=input.next ().charAt (0) ;
         System.out.println ("____") ;
         //*
         length=series.length () ;
         series=series.toLowerCase () ;
-        letter=Character.toLowerCase (letter) ;
+        word=Character.toLowerCase (word) ;
         //*
         for (i=0;i<length;i++) {
-            if (series.charAt (i)==letter) {
+            if (series.charAt (i)==word) {
                 counter++ ;
             }
         }
         //*
-        System.out.println ("The Letter \""+letter+"\" is used \""+counter+"\" times in \""+series+"\"") ;
+        System.out.println ("The word \""+word+"\" is used \""+counter+"\" times in \""+series+"\"") ;
         System.out.println ("________") ;
     }
 }
