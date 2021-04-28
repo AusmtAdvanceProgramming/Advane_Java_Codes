9352ef04617ce9dbccb1f3ed4531cc07ccf92d36
Kamyab Ashouri <80399697+KamyabAshouri@users.noreply.github.com>
2021-04-28 15:40:44 +0430

    Edit Q4.java
    Edit Q4.java

--- AUSMT_Winter_99/src/team4/quiz2/Q4.java
+++ AUSMT_Winter_99/src/team4/quiz2/Q4.java
@@ -16,7 +16,7 @@ public class Q4 {
         int counter=0 ;
         int i=1 , j=1 ;
         //***
-        for (i=1;flag<=40;i++) {
+        for (i=1;flag<40;i++) {
             for (j=1;j<=i;j++) {
                 if (i%j==0) {
                     counter++ ;
