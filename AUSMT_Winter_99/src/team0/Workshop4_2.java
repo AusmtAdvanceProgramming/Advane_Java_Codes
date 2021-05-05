67986ee2e11845c6572b3635569a1208b1d25ec6
vernamusic <vernamusic@github.com>
2021-05-05 13:05:53 +0430

    workshop4team0
    انجام شد . غفاری و طاهری

---
+++ AUSMT_Winter_99/src/team0/Workshop4_2.java
@@ -0,0 +1,26 @@
+
+package team0;
+
+ import javax.swing.JOptionPane;
+
+public class Workshop4_2 {
+    public static void main(String[] args) {
+        int arraySize;
+        String size = JOptionPane.showInputDialog(null,
+                "Please enter number of names: ",
+                "Specifying List Size",
+                JOptionPane.QUESTION_MESSAGE) ;
+        arraySize = Integer.parseInt(size);
+        String[] myList = new String[arraySize];
+        
+        for(int i = 0; i < myList.length; i++){
+            myList[i] = JOptionPane.showInputDialog(null,
+                    String.format("Please enter the name #%d:",i),
+                    "#" + i);
+        }
+        for(int i = 0; i < myList.length; i++){
+            System.out.println(myList[i]);
+        }
+    }
+    
+}
