850e59c6c5ffa074bbffc1ea4d03bfc03cb5610e
hniehadian <79898625+hniehadian@users.noreply.github.com>
2021-05-05 21:09:43 +0430

    Update workshop4_3.java

--- AUSMT_Winter_99/src/team1/workshp4/workshop4_3.java
+++ AUSMT_Winter_99/src/team1/workshp4/workshop4_3.java
@@ -1,14 +1,48 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Haniyeh Hadiyan _ Hossein Reahi
  */
 package team1.workshp4;
 
-/**
- *
- * @author Win 10
- */
+import java.util.Scanner;
+
 public class workshop4_3 {
     
-}
+    public static void main (String[] args) {
+        
+        char[] string0 = new char[5];
+        char[] string1;
+        fillArray(string0);
+        
+        System.out.print("out of the method");
+        System.out.println(string0);
+        
+        System.out.print("returned array :");
+        string1= returnArray(10);
+        System.out.println(string1);
+        
+       }
+    //------------------------------------------
+    
+    public static void fillArray(char[] string){
+        
+        for(int i=0; i<string.length; i++){
+            string[i] = (char)('a'+ Math.random()*('z'-'a' + 1));
+        }
+        System.out.print("In method");
+        System.out.println(string);
+        
+    }
+    //------------------------------------------
+    
+    public static char[] returnArray(int nom){
+        
+       char[] string = new char[nom];
+       
+       for(int i=0; i<string.length; i++){
+           
+          string[i] =(char)('a'+Math.random()*('z'-'a'+1));
+       }
+       return string;
+    }
+        
+}//The end
