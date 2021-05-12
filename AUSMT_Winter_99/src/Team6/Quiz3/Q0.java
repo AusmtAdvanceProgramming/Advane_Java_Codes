6849a8a96fde5bb56f9b2f29a090ec982776b36e
Mahdi Adabi <75334894+mahdiadabi@users.noreply.github.com>
2021-05-12 12:23:47 +0430

    Team6.quiz3

---
+++ AUSMT_Winter_99/src/Team6/Quiz3/Q0.java
@@ -0,0 +1,44 @@
+
+package team6.quiz3;
+
+/**
+ * @author Mahdi
+ * 
+ */
+import javax.swing.JOptionPane;
+
+public class Q0 {
+    public static void main(String[] args) {
+        char loop = 'y';
+        double number = 0;
+        double maximum = Double.NEGATIVE_INFINITY;
+        double minimum = Double.POSITIVE_INFINITY;
+        double sum = 0;
+        int i = 1;
+        while (loop == 'y') {
+            String input1 = JOptionPane.showInputDialog(null, "Enter your intended number: ", "Number input",JOptionPane.QUESTION_MESSAGE);
+            number = Double.parseDouble(input1);
+
+            sum += number;
+            if (number >= maximum) {
+                maximum = number;
+            } 
+            if (number <= minimum) {
+                minimum = number;
+            } 
+
+            String input2 = JOptionPane.showInputDialog(null, "Do you want to continue ?!", "Letter input",
+                    JOptionPane.QUESTION_MESSAGE);
+            input2 = input2.toLowerCase();
+            loop = input2.charAt(0);
+        } 
+
+        System.out.println();
+        System.out.println("The largest number is " + maximum + "\n");
+
+        System.out.println("The smallest number is " + minimum + "\n" );
+
+        System.out.println("The sum of the numbers is " + sum + "\n");
+
+    }
+} 
---
+++ AUSMT_Winter_99/src/Team6/Quiz3/Q4.java
@@ -0,0 +1,87 @@
+
+package team6.quiz3;
+
+/**
+ * @author Mahdi
+ * 
+ */
+import javax.swing.JOptionPane;
+
+
+public class Q4 {
+  
+  public static int binarySearch(int[] array, int number,int low,int high){
+    int middle;
+    
+    while(low <= high){
+        middle =(low + high) / 2;
+        if(number == array[middle]){
+            return middle;
+            }
+        
+        else if (number < array[middle])
+            high = middle - 1;
+        
+        else  
+            low = middle + 1; 
+    }
+    return -1;
+  } 
+
+    public static void main(String[] args) {
+      int arraySize = 100;
+      int[] array = new int[arraySize];
+      int number = 0, result = 0, hold = 0;
+      
+      //giving value
+      for (int i = 0; i < arraySize; i++){
+          array[i] = (int)(Math.random() * 101);
+      }
+              
+      //for orgenizing
+      for (int j = 0; j < arraySize -1; j++){
+          
+          for(int i = 0; i < arraySize -1;i++){
+              
+              if(array[i] > array[i + 1]){
+                  hold = array[i];
+                  array[i] = array[i+1];
+                  array[i+1] = hold;
+              }
+          }
+      }
+
+      String input = JOptionPane.showInputDialog(null, "Enter your intended number: ", "Number search",JOptionPane.QUESTION_MESSAGE);
+      number = Integer.parseInt(input);
+
+      result = binarySearch(array, number, 0, arraySize - 1);
+      
+      if (result != -1){
+              for(int i = 0; i < array.length; i++){
+              if ( i % 10 == 0){
+                  System.out.println();
+              }
+              System.out.print(array[i] + "\t");
+              
+          }
+              int place;
+              place = result +1;       
+              System.out.println("\n\n*** " + number + " was found in index " + place + " ***");
+      }
+      else {
+              for(int i = 0; i < array.length; i++){
+              if ( i % 10 == 0){
+                  System.out.println();
+              }
+              System.out.print(array[i] + "\t");
+              
+          }
+              System.out.println("\n*** " + number + " was not found in array ***");
+      }
+      
+      System.out.println();
+      System.out.println("Done.");
+
+  }
+  
+}
