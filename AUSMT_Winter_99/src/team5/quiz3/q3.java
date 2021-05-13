5848106608ce825c3932fa9e4c80e7563b88c264
mohammadreza movaheadniya <mr.movaheadniya.81@gmail.com>
2021-05-13 15:16:22 +0430

    team5.quiz3

---
+++ AUSMT_Winter_99/src/team5/quiz3/q3.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team5.quiz3;
+
+import javax.swing.JOptionPane ;
+/**
+ *
+ * @author mohammdreza
+ */
+public class q3 {
+    public static void main (String[] args) {
+        int number ;
+        int random ;
+        int i=1 ;
+        //...
+        String input=JOptionPane.showInputDialog (null,"Enter the desired number of characters please :","Number input",JOptionPane.QUESTION_MESSAGE) ;
+        number=Integer.parseInt (input) ;
+        //...
+        System.out.println ("____________________") ; 
+        System.out.print ("Your string is : ") ;
+        //...
+        for (i=1;i<=number;i++) {
+            random=(int) (Math.random ()*3) ;
+            //...
+            switch (random) {
+                case 0 :
+                    System.out.print ((int) (Math.random ()*10)) ;
+                    break ;
+                case 1 :
+                    System.out.print ((char) ('a'+Math.random ()*('z'-'a'+1))) ;
+                    break ;
+                case 2 :
+                    System.out.print ((char) ('A'+Math.random ()*('Z'-'A'+1))) ;
+                    break ;
+                } // end of first switch
+        } // end of first for
+        //...
+        System.out.println ("\n____________________") ;
+    } // end of main () method
+} // end of q3 class    
+
