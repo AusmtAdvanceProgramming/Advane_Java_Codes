06bdc0afe480804fc73328023d5170ff5f7f3152
Soroush <soroush.ashkar@gmail.com>
2021-05-20 01:13:02 +0430

    team7.workshop5

---
+++ AUSMT_Winter_99/src/Team7/workshop5/Student.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Team7.workshop5;
+
+/**
+ *
+ * @author soroush
+ */
+public class Student {
+    
+    private String FirstName;
+    private String LastName;
+    private String[] ListOflessons;
+    private String NameOfUni;
+    private String Majorname;
+    private int SemesterNumber;
+    private int StudentCode;
+    
+    public Student(){
+    }
+    
+    public String getFirstName (String Fname){
+        FirstName = Fname;
+        return FirstName;
+    }
+    public String getLastName (String Lname){
+        LastName = Lname;
+        return LastName;
+    }
+    public String[] getListOflessons (String[] LofL){
+        ListOflessons = LofL;
+        return ListOflessons;
+    }
+    public String getNameOfUni (String NofU){
+        NameOfUni = NofU;
+        return NameOfUni;
+    }
+    public String getMajorname (String MName){
+        Majorname = MName;
+        return Majorname;
+    }
+    public int getSemesterNumber (int Snum){
+        SemesterNumber = Snum;
+        return SemesterNumber;
+    }
+    public int getStudentCode (int StuNum){
+        StudentCode = StuNum;
+        return StudentCode;
+    }
+    
+    public void study(){
+    }
+    public void TakeExams(){
+    }
+    
+}
