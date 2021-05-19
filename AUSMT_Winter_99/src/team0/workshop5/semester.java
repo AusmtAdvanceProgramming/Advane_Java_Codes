2553db81a610667430681c49c10c95f97f9effe4
Nika Taheri <79975793+NikaTaheri@users.noreply.github.com>
2021-05-20 00:43:45 +0430

    Update semester.java

--- AUSMT_Winter_99/src/team0/workshop5/semester.java
+++ AUSMT_Winter_99/src/team0/workshop5/semester.java
@@ -5,10 +5,27 @@ public class semester {
     private int numberOfLesson;
     private String nameOfUnit;
     private String listOfStudent;
-    private int numberOfStudents;
-    //****************************************88
+    private int numberOfStudent;
+    //****************************************
     public semester(){
     }
+    public int getNumberOfUnit(int a){
+        a=numberOfLesson;
+        return numberOfLesson;
+    }
+      public String getListOfStudent(String b){
+        b=listOfStudent;
+        return listOfStudent;
+    }
+       public String getNameOfUnit(String c){
+        c=nameOfUnit;
+        return nameOfUnit;
+    }
+        public int getNumberOfstudents(int d){
+        d=numberOfStudent;
+        return numberOfStudent;
+    }
+      public void studing(){}
     
     
     
